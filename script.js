ccf969de13bcdd35955af6493c590c720778b4d4
ailtonjunior11 <ailtonjunior.dev.frontend@gmail.com>
2024-10-31 08:33:54 -0300

    firist commit TECNOFIX

---
+++ script.js
@@ -0,0 +1,5 @@
+// Config menu mobile
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.getElementById("nav-links").classList.toggle("show");
+});
