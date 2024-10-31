c2e8fa96a2cf2d5892109780762c98b190938585
ailtonjunior11 <ailtonjunior.dev.frontend@gmail.com>
2024-10-31 14:39:27 -0300

    Atualização index.html, script.js, about.css

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ document.getElementById("menu-toggle").addEventListener("click", function () {
   this.classList.toggle("active");
   document.getElementById("nav-links").classList.toggle("show");
 });
+
+// Config scrollreveal
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".home__content", {
+  origin: "left",
+  distance: "100px",
+  duration: 1500,
+});
+
+sr.reveal(".about__text", {
+  origin: "top",
+  distance: "130px",
+  duration: 1500,
+});
